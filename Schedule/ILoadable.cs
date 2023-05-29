a2a01d6fb784735889f7f88a096c3f1ecabd597f
mikudeath <yaneadolfik@gmail.com>
2023-05-29 09:53:17 +0900

    moved files

--- Schedule/ILoadable.cs
+++ Schedule/ILoadable.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Schedule.Interfaces
+namespace Schedule
 {
     internal interface ILoadable<T>
         where T : ISaveable
--- Schedule/ISaveable.cs
+++ Schedule/ISaveable.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Schedule.Interfaces
+namespace Schedule
 {
     internal interface ISaveable
     {
