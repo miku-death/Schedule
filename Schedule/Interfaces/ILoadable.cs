8c183e392e4d809b8b107eaa8456d5ff1c37dc55
mikudeath <yaneadolfik@gmail.com>
2023-05-29 09:50:48 +0900

    save/load interfaces

---
+++ Schedule/Interfaces/ILoadable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule.Interfaces
+{
+    internal interface ILoadable<T>
+        where T : ISaveable
+    {
+        public T Load(string path);
+    }
+}
---
+++ Schedule/Interfaces/ISaveable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule.Interfaces
+{
+    internal interface ISaveable
+    {
+        public void Save(ISaveable o);
+        public void SaveChanges(ISaveable o, string path);
+    }
+}
